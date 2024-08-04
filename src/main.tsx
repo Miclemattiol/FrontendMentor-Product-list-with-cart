065f242cbc5508f259e64a692d7c01b2ae9e90c2
Miclemattiol <michael@mmattiolo.com>
2024-08-04 09:25:28 +0200

    Tmp readme and favicon

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.scss';
 import CartProvider from './providers/cartProvider.tsx';
+import Favicon from 'react-favicon';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
+		<Favicon url='/assets/images/favicon-32x32.png' />
 		<CartProvider>
 			<App />
 		</CartProvider>
